b84ec0664d4dac1bde99b2c31342be48ce083532
wxf <809602518@qq.com>
2015-11-30 18:32:36 +0800

    修改

--- public/user/js/index.js
+++
@@ -1,8 +0,0 @@
-var a = require('./a');
-// var b = require('./b');
-// var d = require('./d');
-// console.log(d());
-require(['./d','./b'],function(d,b){
-	console.log(d(),b());
-	var c = require('./c'); 
-});
--- public/user/webpack.config.js
+++ public/user/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
     //其它解决方案配置
     resolve: {
         // root: 'E:/github/flux-example/src', //绝对路径
-        extensions: ['', '.js', '.json', '.scss'],
+        extensions: ['', '.js', '.json', '.scss']
         // alias: {
         //     AppStore : 'js/stores/AppStores.js',
         //     ActionType : 'js/actions/ActionType.js',
